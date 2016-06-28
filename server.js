48439a85aff03f01d8bd14dc65718699614b1c77
cheng fang <cf1497@nyu.edu>
2016-06-28 18:09:24 -0400

    login fix

--- server.js
+++ server.js
@@ -2,16 +2,18 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var session  = require('express-session');
+var express = require('express');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var app      = express();
-var port     = process.env.PORT || 8080;
+var app = express();
+var port = process.env.PORT || 8080;
 
 var passport = require('passport');
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
+var favicon = require('serve-favicon');
+
 
 // configuration ===============================================================
 // connect to our database
@@ -24,7 +26,7 @@ require('./config/passport')(passport); // pass passport for configuration
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 app.use(bodyParser.json());
 
@@ -32,14 +34,24 @@ app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
 app.use(session({
-	secret: 'vidyapathaisalwaysrunning',
-	resave: true,
-	saveUninitialized: true
- } )); // session secret
+    secret: 'vidyapathaisalwaysrunning',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
+
+// static resuources ======================================================================
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use('/', express.static(__dirname + '/public'));
+app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/jsc', express.static(__dirname + '/public/jsc'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/fonts', express.static(__dirname + '/public/fonts'));
+app.use('/audio', express.static(__dirname + '/public/audio'));
+app.use('/video', express.static(__dirname + '/public/video'));
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
